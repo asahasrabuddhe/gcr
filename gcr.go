45142b4f5facf8de022c7ae683a69cc5b0e1d14d
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-02-24 04:39:29 +0000

    fork process to be able to clone namespaces

--- gcr.go
+++ gcr.go
@@ -12,6 +12,29 @@ func main() {
 		log.Fatal("Usage: gcr run <image> <command>")
 	}
 
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "fork":
+		fork()
+	default:
+		log.Fatal("Usage: gcr run <image> <command>")
+	}
+}
+
+func run() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"fork"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fork() {
 	cmd := exec.Command(os.Args[3], os.Args[4:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
